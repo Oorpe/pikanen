bc2421b066174f53383c64800ea8fe279921e293
Roope <roope.a.tervo@gmail.com>
2018-01-23 03:15:32 -0500

    more usage stuff

--- index.js
+++ index.js
@@ -1,23 +1,95 @@
 #!/usr/bin/env node
 var fs = require('fs');
+//minimist for basic arg parsing
 var argv = require('minimist')(process.argv.slice(2));
 // console.dir(argv);
+//list known flags and keywords, all others are treated as errors and thrown
+var known = [
+    "a",
+    "alias",
+    "p",
+    "port",
+    "_"
+]
 
-//unnamed contains the core path arguments
-var paths = "";
-if(argv._.length > 0){
-    for(var path of argv._){
-     paths += "app.use('/" + path + "', express.static('" + path + "'))" + "\n";
+var keys = Object.keys(argv);
+
+keys.forEach((key)=>{
+    if(!known.includes(key)){
+        var str = "weird flag detected: -" + key + ", are you sure you put two dashes for '--alias'?";
+        throw(str)
     }
+})
+
+//default to 3000
+var port = argv.port || argv.p || 3000;
+
+var paths = [];
+
+//on a or alias all filenames are to be interpreted as aliasable, check for = signs
+if(argv.a || argv.alias){
+    paths.push(argv.alias || argv.a);
+    paths = paths.concat(argv._);
 }
 
+var parsedpaths = paths.map(path => {
+    // console.log(path)
+    return checkForEquals(path)
+    // console.log(path)
+    // return path;
+})
+
+console.log(parsedpaths)
+//unnamed contains the core path arguments
+var staticServes = "";
+
+if(parsedpaths.length > 0){
+    for(var path of parsedpaths){
+     staticServes += "app.use('" + leadingSlash(path.alias) + "', express.static('" + path.dir + "'))" + "\n";
+    }
+}
+console.log(staticServes)
+// //
+var dirs = parsedpaths.map(x => x.dir);
+var aliases = parsedpaths.map(x => {
+    return "localhost:" + port + leadingSlash(x.alias)
+})
+console.log(aliases)
 var res = `
 const express = require('express')
 const app = express()
-`+paths+`
-app.listen(3000, function(){console.log('folder/s `+argv._+` is/are served at localhost:3000/`+argv._+`')})`;
+`+staticServes+`
+app.listen(`+port+`, function(){console.log('dirs [`+ dirs +`] served at `+aliases+`')})`;
 
-fs.writeFile("quickit-index.js", res, function(){
+fs.writeFile("pikanen-index.js", res, function(){
     var cp = require('child_process');
-    var n = cp.fork('./quickit-index.js');
+    var n = cp.fork('./pikanen-index.js');
 });
+
+/**
+    check for equals signs, split to dir and alias strings
+*/
+function checkForEquals(str){
+    // console.log("hei")
+    var i = str.indexOf("=");
+    var res;
+    if(i != -1){
+        res = {dir: str.slice(i+1), alias: str.slice(0, i)}
+    }else{
+        //no = sign
+        res = {dir: str, alias: str}
+    }
+    // console.log(res);
+    return res;
+}
+
+/**
+    add leading slashes when necessary
+*/
+function leadingSlash(str){
+    if(str[0] == "/"){
+        return str;
+    }else{
+        return "/"+str;
+    }
+}
