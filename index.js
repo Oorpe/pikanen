b4624d82c79ce9b50fe15a4f2dd2649a22b428fc
Roope <roope.a.tervo@gmail.com>
2018-01-23 04:19:39 -0500

    added root flag

--- index.js
+++ index.js
@@ -5,11 +5,10 @@ var argv = require('minimist')(process.argv.slice(2));
 // console.dir(argv);
 //list known flags and keywords, all others are treated as errors and thrown
 var known = [
-    "a",
-    "alias",
-    "p",
-    "port",
-    "_"
+    "r","root", //specify server root path
+    "p","port", //specify the port to use
+    "_",
+    "s","silent" //only generate express server file, don't run it
 ]
 
 var keys = Object.keys(argv);
@@ -23,23 +22,25 @@ keys.forEach((key)=>{
 
 //default to 3000
 var port = argv.port || argv.p || 3000;
+var root = argv.root || argv.r || "";
+root = leadingSlash(root);
+
+// if(argv.r || argv.root){
+//     root = argv.root || argv.r;
+//     // port = "" + portnum + leadingSlash(argv.root || argv.r);
+// }
 
 var paths = [];
 
-//on a or alias all filenames are to be interpreted as aliasable, check for = signs
-if(argv.a || argv.alias){
-    paths.push(argv.alias || argv.a);
-    paths = paths.concat(argv._);
-}
+paths = paths.concat(argv._);
 
 var parsedpaths = paths.map(path => {
-    // console.log(path)
     return checkForEquals(path)
-    // console.log(path)
-    // return path;
 })
 
-console.log(parsedpaths)
+var nl = "\n";
+
+// console.log(parsedpaths)
 //unnamed contains the core path arguments
 var staticServes = "";
 
@@ -48,22 +49,25 @@ if(parsedpaths.length > 0){
      staticServes += "app.use('" + leadingSlash(path.alias) + "', express.static('" + path.dir + "'))" + "\n";
     }
 }
-console.log(staticServes)
+// console.log(staticServes)
 // //
 var dirs = parsedpaths.map(x => x.dir);
 var aliases = parsedpaths.map(x => {
-    return "localhost:" + port + leadingSlash(x.alias)
+    var some =  nl + "localhost:" + port + root + leadingSlash(x.alias)
+    // console.log(some);
+    return some
 })
-console.log(aliases)
+// console.log(aliases)
 var res = `
 const express = require('express')
 const app = express()
 `+staticServes+`
-app.listen(`+port+`, function(){console.log('dirs [`+ dirs +`] served at `+aliases+`')})`;
+app.listen(`+port+", function(){console.log(`dirs ["+ dirs +"] served at " + aliases + "`)})"
 
 fs.writeFile("pikanen-index.js", res, function(){
     var cp = require('child_process');
     var n = cp.fork('./pikanen-index.js');
+    // console.log(res);
 });
 
 /**
